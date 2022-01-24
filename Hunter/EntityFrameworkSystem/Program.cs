18fc4e28b35b009dfbbc0f9752311fdbb5ad23df
giordanap <40145046+giordanap@users.noreply.github.com>
2022-01-24 06:21:11 -0500

    finish

---
+++ Hunter/EntityFrameworkSystem/Program.cs
@@ -0,0 +1,180 @@
+﻿using BD;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // La desventaja principal de EF es que tarda con consultas masivas
+
+            // Esto me permite entregar la cadena de conexion, así ya no sera
+            // por defecto y no entrara en el condicional de la clase
+            // CsharpDBContext
+            DbContextOptionsBuilder<CsharpDBContext> optionsBuilder =
+                new DbContextOptionsBuilder<CsharpDBContext>();
+            optionsBuilder.UseSqlServer("Server=GIORDAN-PC; Database=CsharpDB; Trusted_Connection=True;");
+
+            // Crea un universo unico de existencia de mis datos
+            // Asi using crea el dispose por ti
+            // Using solo trabaja con clases que implementen el
+            // metodo dispose
+            //using (CsharpDBContext context = new CsharpDBContext(optionsBuilder.Options))
+            //{
+            //    var beers = context.Beers.ToList();
+
+            //    foreach (var beer in beers)
+            //    {
+            //        Console.WriteLine(beer.Name);
+            //    }
+            //}
+
+            bool again = true;
+            int op = 0;
+
+            do
+            {
+                ShowMenu();
+
+                Console.WriteLine("Elige una opcion: ");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op) 
+                {
+                    case 1:
+                        Show(optionsBuilder);
+                        break;
+                    case 2:
+                        Add(optionsBuilder);
+                        break;
+                    case 3:
+                        Edit(optionsBuilder);
+                        break;
+                    case 4:
+                        Delete(optionsBuilder);
+                        break;
+                    case 5:
+                        again = false;
+                        break;
+                }
+            } while (again);
+ 
+        }
+
+        public static void Show(DbContextOptionsBuilder<CsharpDBContext> optionsBuilder)
+        {
+            Console.Clear();
+            Console.WriteLine("Cervezas de la base de datos:");
+            using (var context = new CsharpDBContext(optionsBuilder.Options))
+            {
+
+                //List<Beer> beers = context.Beers.ToList();
+                // Hacemos uso de la expresion Lamda
+                // Include hace un join con la marca
+                //List<Beer> beers = context.Beers.OrderBy(b=>b.Name).ToList();
+                List<Beer> beers2 = (from b in context.Beers
+                                     where b.BrandId == 2
+                                     orderby b.Name
+                                     select b).Include(b=>b.Brand).ToList();
+
+                foreach (var beer in beers2)
+                {
+                    Console.WriteLine($"Id: {beer.Id} Nombre: {beer.Name} Marca: {beer.Brand}");
+                }
+            }
+        }
+
+        public static void Add(DbContextOptionsBuilder<CsharpDBContext> optionsBuilder)
+        {
+            Console.Clear();
+            Console.WriteLine("Agregar nueva cerveza");
+            Console.WriteLine("Escriba el nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Escribe el id de la marca:");
+            int brandId = int.Parse(Console.ReadLine());
+
+            using (var context = new CsharpDBContext(optionsBuilder.Options))
+            {
+                Beer beer = new Beer()
+                {
+                    Name = name,
+                    BrandId = brandId
+                };
+
+                context.Add(beer);
+                context.SaveChanges();
+            }
+
+        }
+
+        public static void Edit(DbContextOptionsBuilder<CsharpDBContext> optionsBuilder)
+        {
+            Console.Clear();
+            Show(optionsBuilder);
+            Console.WriteLine("Editar Cerveza");
+            Console.WriteLine("Escribe el id de tu cerveza a editar: ");
+            int id = int.Parse(Console.ReadLine());
+
+            using (var context = new CsharpDBContext(optionsBuilder.Options))
+            { 
+           
+                Beer beer = context.Beers.Find(id);
+                if (beer != null)
+                {
+                    Console.WriteLine("Escribe el nombre: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Escribe el id de la marca: ");
+                    int brandId = int.Parse(Console.ReadLine());
+
+                    beer.Name = name;
+                    beer.BrandId = brandId;
+                    context.Entry(beer).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("La cerveza no existe");
+                }
+            }
+
+        }
+
+        public static void Delete(DbContextOptionsBuilder<CsharpDBContext> optionsBuilder)
+        {
+            Console.Clear();
+            Show(optionsBuilder);
+            Console.WriteLine("Eliminar Cerveza");
+            Console.WriteLine("Escribe el id de tu cerveza a eliminar: ");
+            int id = int.Parse(Console.ReadLine());
+
+            using (var context = new CsharpDBContext(optionsBuilder.Options))
+            { 
+                Beer beer = context.Beers.Find(id);
+                if (beer != null)
+                {
+                    context.Beers.Remove(beer);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("La cerveza no existe");
+                }
+            
+            }
+        }
+
+        public static void ShowMenu()
+        {
+            Console.WriteLine("\n-------Menu-------");
+            Console.WriteLine("1.- Mostrar");
+            Console.WriteLine("2.- Agregar");
+            Console.WriteLine("3.- Editar");
+            Console.WriteLine("4.- Eliminar");
+            Console.WriteLine("5.- Salir");
+        }
+    }
+}
---
+++ Hunter/Hunter/LearningCS/IX.IntroFunctionalProgramming/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+// Funcion de primer orden 
+// Funcion de orden superior, es una funcion que puede recibir
+// como parametro otras funciones, estas funciones que son
+// recibidas son funciones de primer orden (pe: Sum, Mul, etc.)
+// osea, que una variable que contiene a una funcion tambien
+// puede ser enviado como parametro a una funcion
+
+// Estamos guardando en una variable una funcion
+Operation mySum = Functions.Sum;
+
+//System.Console.WriteLine(mySum(2, 3));
+mySum = Functions.Mul;
+//System.Console.WriteLine(mySum(2, 3));
+
+Show cw = Console.WriteLine;
+cw += Functions.ConsoleShow;
+//cw("Hola mundo");
+//Functions.Some("Giordan", "Arredondo", cw);
+
+// El delegado generico nos ayuda a poder ahorra codigo a la
+// hora de definir los delegados cuando se definen los parametros
+
+// Usaremos las regiones (que no es codigo) para poder visualizar mejor
+
+#region Action
+string hi = "Hola";
+// Es una funcion que recibe parametros, pero no regresa nada
+// Si se escribe solo Action es porque no recibe parametros
+Action<string> showMessage = Console.WriteLine;
+// Funciones Lambda (en JS son arrow function), son formas de
+// escribir funciones anonimas de manera rapida y practica, sin crear
+// un nombre.
+Action<string, string> showMessage2 = (a, b) => {
+    Console.WriteLine($"{hi} {a} {b}");
+};
+Action<string, string, string> showMessage3 = (a, b, c) => Console.WriteLine($"{a} {b} {c}");
+//showMessage2("Giodan", "Arredondo");
+//showMessage3("Giodan", "Arredondo", "Dev");
+//Functions.SomeAction("Gabriel", "Arredondo", (a) =>
+//{
+//    Console.WriteLine("Soy una expresion lambda " + a);
+//});
+//Functions.SomeAction("Gabriel", "Arredondo", showMessage);
+#endregion
+
+#region Func
+// La diferencia entre Func y Action es que Action te sirve para
+// genericos, pero no devuelve nada, lo que pasa con las funciones
+// es que sí retornan algo. Tambien puede retornar algo, siendo el
+// ultimo tipo de dato el tipo de dato de lo que se retornara
+Func<int> numberRandom = () => new Random().Next(0, 100);
+Func<int, string> numberRandomLimit = (limit) => new Random().Next(0, limit).ToString();
+
+//Console.WriteLine(numberRandom());
+//Console.WriteLine(numberRandomLimit(10000));
+#endregion
+
+#region Predicate
+// Estos delegados reciben parametros y devuelven un true o false
+Predicate<string> hasSpaceOrA = (word) => word.Contains(" ") || word.ToUpper().Contains("A");
+Console.WriteLine(hasSpaceOrA("beear"));
+Console.WriteLine(hasSpaceOrA("p ati to"));
+
+var words = new List<string>()
+{
+    "beer",
+    "patita",
+    "sandia",
+    "hola mundo",
+    "c#"
+};
+var wordsNew = words.FindAll((w) => !hasSpaceOrA(w));
+foreach (var w in wordsNew) Console.WriteLine(w);
+#endregion
+
+#region Delegados
+// El delegado definira la estructura de la funcion
+delegate int Operation(int a, int b);
+// Para poder utilizar el delegado en el ambito de Functions
+// debemos volverlo publico
+public delegate void Show(string message);
+#endregion
+
+public class Functions
+{
+    public static int Sum(int x, int y) => x + y;
+    public static int Mul(int num1, int num2) => num1 * num2;
+    public static void ConsoleShow(string m) => Console.WriteLine(m.ToUpper());
+    public static void Some(string name, string lastName, Show fn)
+    {
+        Console.WriteLine("Hago algo al inicio");
+        fn($"Hola {name} {lastName}");
+        Console.WriteLine("Hago algo al final");
+    }
+    public static void SomeAction(string name, string lastName, Action<string> fn)
+    {
+        Console.WriteLine("Hago algo al inicio");
+        fn($"Hola {name} {lastName}");
+        Console.WriteLine("Hago algo al final");
+    }
+}
