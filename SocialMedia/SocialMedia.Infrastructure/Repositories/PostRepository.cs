1f8a1984f9bded28a30c370493ac4abc51c59e68
giordanap <40145046+giordanap@users.noreply.github.com>
2022-02-05 00:14:01 -0500

    Fix Post Repository

--- SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using SocialMedia.Core.Entities;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace SocialMedia.Infrastructure.Repositories
 {
-    internal class PostRepository
+    public class PostRepository
     {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+
+            return posts;
+        }
     }
 }
